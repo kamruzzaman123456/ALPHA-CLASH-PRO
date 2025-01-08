3f5ab0d98a502d9b410f17b6dcbecc2cd21694a2
kamruzzaman123456 <mdkamruzzamanmajumder@gmail.com>
2025-01-09 00:44:47 +0600

    game over

--- alpha-clash.js
+++ alpha-clash.js
@@ -10,6 +10,87 @@
 
 // }
 
+// continue playing
+
+function handleKeybuttonPrass(event){
+
+  const playerprass = event.key;
+  console.log('player prass',playerprass);
+
+  // click Esc to skip game 
+
+  if(playerprass==='Escape'){
+    gameover();
+  }
+
+  const currentAlphabetElemant = document.getElementById('carant-alphabet');
+
+   const currentAlphabet =currentAlphabetElemant.innerText
+   
+   const expectedAlphabet = currentAlphabet.toLocaleLowerCase();
+  console.log(playerprass,expectedAlphabet);
+
+//  chake match or not
+if(playerprass===expectedAlphabet){
+  console.log('You got point')
+
+  const currentscore = getTextElemantValueById('current-score')
+  const updatedscore = currentscore + 1;
+
+  setTextElemantValueById('current-score',updatedscore)
+
+
+// // 1-get current score
+//   const currentscoreElemant = document.getElementById('current-score');
+//   const currentscoreText = currentscoreElemant.innerText;
+//   const currentscore = parseInt(currentscoreText);
+
+// // 2-incrasc current score
+//   const newscore = currentscore + 1;
+
+// // 3-set updata score
+
+// currentscoreElemant.innerText = newscore
+
+  console.log('You have prass corectly',expectedAlphabet)
+  removeBackgroundColor(expectedAlphabet);
+  continueGame();
+}
+
+else{
+  console.log('You lost a life')
+
+  const currentlife = getTextElemantValueById('current-life')
+  const updatedlife = currentlife - 1;
+
+  setTextElemantValueById('current-life',updatedlife)
+  
+
+ 
+  if(updatedlife===0){
+    gameover()
+  }
+
+
+
+//   // 1-get current life
+//   const currentlifeElemant = document.getElementById('current-life');
+//   const currentlifeText = currentlifeElemant.innerText;
+//   const currentlife = parseInt(currentlifeText);
+
+// // 2-incrasc current score
+//   const radiouslife = currentlife - 1;
+
+// // 3-set updata score
+
+// currentlifeElemant.innerText = radiouslife ;
+
+
+}
+}
+
+document.addEventListener('keyup',handleKeybuttonPrass);
+
 
 function continueGame(){
 
@@ -17,16 +98,42 @@ function continueGame(){
 const alphabet = getArandomAlphabet()
 //  set random generated alphabet
 
+console.log('Your random alpha',alphabet);
+
 const carantAlphabet = document.getElementById('carant-alphabet');
 
-carantAlphabet.innerText=alphabet
+carantAlphabet.innerText=alphabet ;
+
 // set background color
 
 setBackgroundColor(alphabet);
+
 }
 
 function play(){
+  // hide every thing only show playground
     hideElemantById('home-scrin');
+    hideElemantById('score-bode');
     showElemantById('playground-scrin');
+
+    // reset life and score
+    setTextElemantValueById('current-life',5)
+    setTextElemantValueById('current-score',0)
     continueGame();
+}
+
+// hidden plaground and display score bode
+
+function gameover(){
+  hideElemantById('playground-scrin');
+  showElemantById('score-bode');
+
+  // set the finel score
+
+  const lestfinelscore = getTextElemantValueById('current-score')
+  setTextElemantValueById('scoreon-bode',lestfinelscore)
+
+  // clear the last selected alphabet
+   const selectedAalphabet = getElemantTextById('carant-alphabet')
+   removeBackgroundColor(selectedAalphabet);
 }
--- autility.js
+++ autility.js
@@ -15,17 +15,51 @@ function setBackgroundColor(elemantId){
 
 }
 
+//  remove Background Color
+function removeBackgroundColor(elemantId){
+    const elemant =document.getElementById(elemantId);
+    elemant.classList.remove('bg-orange-400')
+
+}
+
 
 function getArandomAlphabet(){
-    const alphabetstring ='abcdefghijklmnopqrstuvwxtz/' ;
+    const alphabetstring ='abcdefghijklmnopqrstuvwxytz/' ;
     const alphabets = alphabetstring.split('');
 
     //get random Number
 
-    const randomNumber =Math.random()*27
+    const randomNumber =Math.random()*28
     const index = Math.round(randomNumber);
 
     const alphabet =alphabets[index];
 
     return alphabet ;
+
 }
+
+
+// life and score function
+
+function getTextElemantValueById( elemantId){
+    const elemants = document.getElementById(elemantId);
+    const elemantvalueText = elemants.innerText;
+    const value = parseInt(elemantvalueText);
+    return value;
+}
+
+// set score and value 
+
+function setTextElemantValueById(elemantId,value){
+    const elemants = document.getElementById(elemantId);
+    elemants.innerText = value;
+   
+}
+
+function getElemantTextById(elemantId){
+    const elemant = document.getElementById(elemantId);
+    const text = elemant.innerText;
+    return text ;
+}
+
+
